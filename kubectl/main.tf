8af92e914c799567abed0342340f5377f0173144
Kuno <gustavo.kuno@linx.com.br>
2023-06-28 08:15:25 -0300

    kubectl module

---
+++ kubectl/main.tf
@@ -0,0 +1,25 @@
+data "kubectl_file_documents" "namespace" {
+  content = file("${path.module}/manifests/argocd/namespace.yaml")
+}
+
+data "kubectl_file_documents" "argocd" {
+  content = file("${path.module}/manifests/argocd/install.yaml")
+}
+
+resource "kubectl_manifest" "namespace" {
+  count              = length(data.kubectl_file_documents.namespace.documents)
+  yaml_body          = element(data.kubectl_file_documents.namespace.documents, count.index)
+  override_namespace = "argocd"
+
+  depends_on = [ data.kubectl_file_documents.namespace ]
+}
+
+resource "kubectl_manifest" "argocd" {
+  count              = length(data.kubectl_file_documents.argocd.documents)
+  yaml_body          = element(data.kubectl_file_documents.argocd.documents, count.index)
+  override_namespace = "argocd"
+
+  depends_on = [
+    kubectl_manifest.namespace,
+  ]
+}
