31e733061d6ec412b687284283be7da5dbb93a25
Kuno <gustavo.kuno@linx.com.br>
2023-07-01 17:27:47 -0300

    fix: module kubectl

--- kubectl/main.tf
+++ kubectl/main.tf
@@ -2,24 +2,26 @@ data "kubectl_file_documents" "namespace" {
   content = file("${path.module}/manifests/argocd/namespace.yaml")
 }
 
-data "kubectl_file_documents" "argocd" {
-  content = file("${path.module}/manifests/argocd/install.yaml")
-}
-
 resource "kubectl_manifest" "namespace" {
-  count              = length(data.kubectl_file_documents.namespace.documents)
-  yaml_body          = element(data.kubectl_file_documents.namespace.documents, count.index)
+  #count              = length(data.kubectl_file_documents.namespace.documents)
+  #yaml_body          = element(data.kubectl_file_documents.namespace.documents, count.index)
+  yaml_body = data.kubectl_file_documents.namespace.documents
   override_namespace = "argocd"
 
   depends_on = [ data.kubectl_file_documents.namespace ]
 }
 
+data "kubectl_file_documents" "argocd" {
+  content = file("${path.module}/manifests/argocd/install.yaml")
+}
+
 resource "kubectl_manifest" "argocd" {
-  count              = length(data.kubectl_file_documents.argocd.documents)
-  yaml_body          = element(data.kubectl_file_documents.argocd.documents, count.index)
+  #count              = length(data.kubectl_file_documents.argocd.documents)
+  #yaml_body          = element(data.kubectl_file_documents.argocd.documents, count.index)
+  yaml_body          = data.kubectl_file_documents.argocd.documents
   override_namespace = "argocd"
 
   depends_on = [
-    kubectl_manifest.namespace,
+    kubectl_manifest.namespace
   ]
 }
